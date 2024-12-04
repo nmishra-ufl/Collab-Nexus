5b4b19fd6c7250fdfe63d9546fa4dda6eed3a3d6
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-12-04 02:12:22 -0500

    Update User.java

--- cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/User.java
+++ cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/User.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -46,6 +43,9 @@ public class User {
 	@Column(name = "bio", length = 500)
 	private String bio;
 
+	@ManyToMany(mappedBy = "users") // Reference the relationship defined in Group
+	private Set<Group> groups = new HashSet<>();
+
 	// Getters and Setters
 	public String getSkills() { return skills; }
 	public void setSkills(String skills) { this.skills = skills; }
@@ -59,45 +59,7 @@ public class User {
 	public String getBio() { return bio; }
 	public void setBio(String bio) { this.bio = bio; }
 
-
-	/*public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}*/
+	public Set<Group> getGroups() { return groups; }
+	public void setGroups(Set<Group> groups) { this.groups = groups; }
 
 }
