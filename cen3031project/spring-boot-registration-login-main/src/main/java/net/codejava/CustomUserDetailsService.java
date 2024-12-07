9510a4582d31ed375ca56254d9a08ab877ffca94
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-12-06 22:54:33 -0500

    Update CustomUserDetailsService.java

--- cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/CustomUserDetailsService.java
+++ cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/CustomUserDetailsService.java
@@ -4,19 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepo;
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepo.findByEmail(username);
-		if (user == null) {
-			throw new UsernameNotFoundException("User not found");
-		}
-		return new CustomUserDetails(user);
-	}
 
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found"); // Throws exception if user is not found
+		}
+		return new CustomUserDetails(user); // Wraps the User entity in a UserDetails implementation
+	}
 }
