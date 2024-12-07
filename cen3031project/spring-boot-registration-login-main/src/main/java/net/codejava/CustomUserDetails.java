53d6c09f7891418223d5513a2fefc95e41cde819
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-12-06 22:54:21 -0500

    Update CustomUserDetails.java

--- cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/CustomUserDetails.java
+++ cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/CustomUserDetails.java
@@ -1,55 +1,53 @@
 package net.codejava;
 
-import java.util.Collection;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class CustomUserDetails implements UserDetails {
+import java.util.Collection;
 
+public class CustomUserDetails implements UserDetails {
 	private User user;
-	
+
 	public CustomUserDetails(User user) {
 		this.user = user;
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return null; // Currently, no roles or authorities are implemented
 	}
 
 	@Override
 	public String getPassword() {
-		return user.getPassword();
+		return user.getPassword(); // Returns the user's hashed password
 	}
 
 	@Override
 	public String getUsername() {
-		return user.getEmail();
+		return user.getEmail(); // Uses email as the username
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		return true;
+		return true; // Account expiration not implemented
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		return true;
+		return true; // Account locking not implemented
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		return true;
+		return true; // Credential expiration not implemented
 	}
 
 	@Override
 	public boolean isEnabled() {
-		return true;
+		return true; // User is always enabled
 	}
-	
+
 	public String getFullName() {
 		return user.getFirstName() + " " + user.getLastName();
 	}
-
 }
