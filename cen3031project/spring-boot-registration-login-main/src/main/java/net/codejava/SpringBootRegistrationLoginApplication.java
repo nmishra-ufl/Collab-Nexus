c8a9a11862b1d2cbc9bf540fd0068ed6933a7c1e
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-12-06 22:55:30 -0500

    Update SpringBootRegistrationLoginApplication.java

--- cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
+++ cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
@@ -3,11 +3,18 @@ package net.codejava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for the Spring Boot Registration and Login Application.
+ */
 @SpringBootApplication
 public class SpringBootRegistrationLoginApplication {
 
+	/**
+	 * Entry point of the application.
+	 *
+	 * @param args Command-line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRegistrationLoginApplication.class, args);
 	}
-
 }
