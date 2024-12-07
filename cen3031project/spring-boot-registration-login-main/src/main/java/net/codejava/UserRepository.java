de43891ece7109c72bc73991ade6b561bb23c91b
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-12-06 22:55:51 -0500

    Update UserRepository.java

--- cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/UserRepository.java
+++ cen3031project/spring-boot-registration-login-main/src/main/java/net/codejava/UserRepository.java
@@ -3,8 +3,17 @@ package net.codejava;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+/**
+ * Repository interface for performing database operations on the User entity.
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	/**
+	 * Finds a user by their email.
+	 *
+	 * @param email The email address of the user.
+	 * @return The User object if found, otherwise null.
+	 */
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
 	public User findByEmail(String email);
-	
 }
